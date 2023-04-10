ac4465c25d6e716c5ac77e3d5c7171ebcd071728
Jorge Arroyo Esquivel <jarroyoe@ucdavis.edu>
2023-04-10 10:29:54 -0700

    Figures ready for draft

---
+++ Data Analysis/null_model_analysis.R
@@ -0,0 +1,70 @@
+rm(list = ls())
+library(dplyr)
+library(forecast)
+
+communitySizes <- c(10,40)
+observationErrors <- c("0.0","0.1","0.001")
+trainingSizes <- c(10,30,50)
+
+ARIMAPredictions <- data.frame(CommunitySize = integer(),
+                              ObservationError = double(),
+                              TrainingSize = integer(), 
+                              TimeSeriesID = integer(),
+                              Time = integer(),
+                              PopID = integer(),
+                              LogDensity = double(),
+                              RealValue = double())
+
+ARIMAPredictionsSD <- data.frame(CommunitySize = integer(),
+                                 ObservationError = double(),
+                                 TrainingSize = integer(), 
+                                 TimeSeriesID = integer(),
+                                 Time = integer(),
+                                 LogDensity = double(),
+                                 Lower95 = double(),
+                                 Higher95 = double())
+
+for(i in communitySizes){
+  for(j in observationErrors){
+        for(n in 1:2){
+          currTimeSeries <- read.csv(paste(sep="",
+                                           paste(sep="_","Data/timeSeries_communitySize",i,
+                                                 "observationError",j,"rep",n),".csv"),
+                                     header=FALSE,sep='\t')
+          for(k in trainingSizes){
+            trainingData <- currTimeSeries[,1:k]
+            for(m in 1:i){
+              ARIMAmodel <- auto.arima(as.vector(t(trainingData[m,])))
+              forecasts <- forecast(ARIMAmodel,h = 50)
+              predictions <- c(trainingData[m,k],forecasts$mean)
+              low95 <- c(0,forecasts$lower[,2])
+              hi95 <- c(0,forecasts$upper[,2])
+              totalEntries <- length(predictions)
+              ARIMAPredictions <- rbind(ARIMAPredictions,
+                                        data.frame(CommunitySize = rep(i,totalEntries),
+                                                   ObservationError = rep(as.double(j),totalEntries),
+                                                   TrainingSize = rep(k,totalEntries), 
+                                                   TimeSeriesID = rep(n,totalEntries),
+                                                   Time = k:(k+50),
+                                                   PopID = rep(m,totalEntries),
+                                                   LogDensity = predictions,
+                                                   RealValue = as.vector(t(currTimeSeries[m,k:(k+50)]))))
+              ARIMAPredictionsSD <- rbind(ARIMAPredictionsSD,
+                                          data.frame(CommunitySize = rep(i,totalEntries),
+                                          ObservationError = rep(as.double(j),totalEntries),
+                                          TrainingSize = rep(k,totalEntries), 
+                                          TimeSeriesID = rep(n,totalEntries),
+                                          Time = k:(k+50),
+                                          PopID = rep(m,totalEntries),
+                                          LogDensity = predictions,
+                                          Lower95 = as.vector(t(low95)),
+                                          Higher95 = as.vector(t(hi95))))
+            }
+          }
+        }
+      }
+    }
+
+ARIMAPredictions <- ARIMAPredictions %>% mutate(Residuals = LogDensity-RealValue)
+
+save(ARIMAPredictions,ARIMAPredictionsSD,file="null_test_predictions_and_residuals.RData")
--- nonautoUDEruns.jl
+++ nonautoUDEruns.jl
@@ -3,9 +3,9 @@ include("NODEUtils.jl")
 include("dataGeneration.jl")
 
 #Run conditions
-communitySizes = [10,50]
+communitySizes = [10,40]
 observationErrors = [0,1e-3,1e-1]
-numberofTimeSeries = 4
+numberofTimeSeries = 2
 trainingSizes = [10, 30, 50]
 initialWeightsNumber = 4
 Tmax = 100
@@ -17,6 +17,10 @@ Tmax = 100
         timeSeries = Float32.(readdlm("Data/timeSeries_communitySize_"*string(communitySize)*"_observationError_"*
         string(observationError)*"_rep_"*string(i)*".csv"))
         for j in 1:initialWeightsNumber
+	    #Check to prevent double work
+	    isfile("Models/nonautonomous_UDE_communitySize_"*string(communitySize)*"_observationError_"*
+                    string(observationError)*"_trainingSize_"*string(trainingSize)*"_rep_"*string(i)*"_"*string(j)) && continue
+
             #Training of models
             UDENonAutonomous = denseLayersLux(communitySize+1,[communitySize*3,communitySize*2])
             trainedParamsUDENonAutonomous = trainUDEModel(UDENonAutonomous,knownDynamics,[timeSeries[:,1:trainingSize];collect(1:trainingSize)'],p_true=1)
