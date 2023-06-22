80bcb24c5abc8f6f3d1d68c72544f205c14aa070
Jorge Arroyo Esquivel <jarroyoesquivel@login-srcf.calc.carnegiescience.edu>
2023-06-22 09:20:37 -0700

    Added UDE results with regularization

--- test.jl
+++ test.jl
@@ -1,16 +1,44 @@
-using CUDA, LinearAlgebra, Zygote, Random
+using OrdinaryDiffEq, DiffEqFlux, Lux
+using Optimization, OptimizationOptimisers, OptimizationOptimJL
+using ComponentArrays, Lux, Zygote, Random, StatsBase, LinearAlgebra
+using CUDA
+rng = Random.default_rng()
 
-function loss(X,niters=10)
-    y = randn!(similar(X, size(X, 2)))
-    tmp = X * y
-    for i in 1:niters
-        tmp = X*y
-        tmp = tmp / norm(tmp)
-        y = X' * tmp
-        y = y / norm(y)
-    end
-    return norm(X*y)
+training_data = rand(10,20)
+neuralNetwork = Lux.Chain(Lux.Dense(10,20),Lux.Dense(20,10))
+knownDynamics(x,p,q) = -x
+pinit, st = Lux.setup(rng,neuralNetwork)
+st = st |> Lux.gpu
+p64 = Float64.(Lux.gpu(ComponentArray(pinit)))
+training_data = Lux.gpu(training_data)
+x0 = training_data[:,1] |> Lux.gpu
+
+
+function ude(u,p,t,q)
+        knownPred = convert(CuArray,knownDynamics(u,nothing,q))
+        nnPred = convert(CuArray,first(neuralNetwork(u,p,st)))
+
+        knownPred .+ nnPred
 end
 
-dL(W) = gradient(X->loss(X),W)
-dL(CUDA.rand(3,2))
+nn_dynamics(u,p,t) = ude(u,p,t,nothing)
+prob_nn = ODEProblem(nn_dynamics,x0, (Float64(1),Float64(size(training_data,2))), p64)
+function predict(p, X = x0)
+        _prob = remake(prob_nn, u0 = X, tspan = (Float64(1),Float64(size(training_data,2))), p = p)
+        CUDA.@allowscalar convert(CuArray,solve(_prob, AutoTsit5(Rosenbrock23()), saveat = 1.,
+                abstol=1e-6, reltol=1e-6
+                ))
+end
+
+function loss_function(p)
+	pred = predict(p)
+	loss = sum(abs2,training_data .- pred)/size(training_data,2)
+	return loss
+end
+
+adtype = Optimization.AutoZygote()
+optf = Optimization.OptimizationFunction((x, p) -> loss_function(x), adtype)
+optprob = Optimization.OptimizationProblem(optf, p64)
+
+result_neuralode = Optimization.solve(optprob,
+                                           ADAM(),maxiters=2)
